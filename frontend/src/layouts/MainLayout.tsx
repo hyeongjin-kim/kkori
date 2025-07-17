f9b7093c2bed30d2a3fb13f8321cbdb5be7b4430
hjkim <kims0887@naver.com>
2025-07-17 17:44:38 +0900

    feat: 메인레이아웃 컴포넌트 구현

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+//메인 레이아웃에 있어야 할 것들
+
+import { Outlet } from "react-router-dom";
+
+// 1. 헤더
+function MainLayout() {
+    return (
+        <div>
+            <Header/>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default MainLayout;
