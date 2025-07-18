0db3035ab8a40f5aea8495e99d21e66442b04c6d
hjkim <kims0887@naver.com>
2025-07-18 16:52:55 +0900

    feat: 라우터에 주요 하위 페이지 등록 Co-authored-by: 이찬 diligent0644@gmail.com

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,21 @@ import HomePage from "./pages/homePage/index";
 import SoloPracticePage from "./pages/soloPracticePage/index";
 import PairPracticePage from "./pages/pairPracticePage/index";
 import MainLayout from "./layouts/MainLayout";
+import InterviewQuestionsPage from "./pages/interviewQuestionsPage/index";
+import MyPage from "./pages/myPage/index";
+import LoginPage from "./pages/loginPage/index";
 
 function App() {
   return (
       <Router>
         <Routes>
-            <Route element={<MainLayout />} >
+          <Route element={<MainLayout />} >
             <Route path="/" element={<HomePage />} />
             <Route path="/solo-practice" element={<SoloPracticePage />} />
             <Route path="/pair-practice" element={<PairPracticePage />} />
+            <Route path="/interview-questions" element={<InterviewQuestionsPage />} />
+            <Route path="/my-page" element={<MyPage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Route>
         </Routes>
       </Router>
