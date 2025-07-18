d589e74dff18c7e36e8385603a95e480858d0fec
hjkim <kims0887@naver.com>
2025-07-18 16:37:49 +0900

    test: 메인 레이아웃 테스트 작성 Co-authored-by: 이찬 diligent0644@gmail.com

--- frontend/src/layouts/MainLayout.test.tsx
+++ frontend/src/layouts/MainLayout.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import MainLayout from "./MainLayout";
+import BrowserRouterWrapped from "../components/common/BrowserRouterWrapped";
 
-test("메인 레이아웃에서 헤더를 확인할할 수 있다.", () => {
-    render(<MainLayout />);
+test("메인 레이아웃에서 헤더를 확인할 수 있다.", () => {
+    render(<BrowserRouterWrapped component={<MainLayout />} />);
     expect(screen.getByRole("banner")).toBeInTheDocument();
 });
