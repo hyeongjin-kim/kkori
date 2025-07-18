dd7fcc16369e4c067bf06780ef5da258455b648b
hjkim <kims0887@naver.com>
2025-07-18 16:38:21 +0900

    feat: 메인 레이아웃 구현 Co-authored-by: 이찬 diligent0644@gmail.com

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage/index";
 import SoloPracticePage from "./pages/soloPracticePage/index";
 import PairPracticePage from "./pages/pairPracticePage/index";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route element={<MainLayout />} >
+            <Route element={<MainLayout />} >
             <Route path="/" element={<HomePage />} />
             <Route path="/solo-practice" element={<SoloPracticePage />} />
             <Route path="/pair-practice" element={<PairPracticePage />} />
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 //메인 레이아웃에 있어야 할 것들
 
 import { Outlet } from "react-router-dom";
+import Header from "../components/common/Header";
 
 // 1. 헤더
 function MainLayout() {
