b60fc391349f5d4a677d31984f94811387a5867b
llcodingll <ll.o.llwhkr@gmail.com>
2025-07-23 16:37:56 +0900

    S13P11A707-4 test: 카카오 인증 리다이렉트로 전달된 인가 코드 추출/비추출(400 Bad Request) 테스트 코드 추가

--- backend/kkori/src/main/java/com/kkori/controller/KakaoOAuth2Controller.java
+++ backend/kkori/src/main/java/com/kkori/controller/KakaoOAuth2Controller.java
@@ -1,11 +1,13 @@
 package com.kkori.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class KakaoOAuth2Controller {
 
