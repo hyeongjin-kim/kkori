823f32e1b83aa6ce04a11804893fff092ae30f2d
hjkim <kims0887@naver.com>
2025-07-23 16:15:38 +0900

    test: 로그인 버튼 테스트 구현

---
+++ frontend/src/components/loginPage/LoginButton.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import LoginButton from './LoginButton';
+import MemoryRouterWrapped from '../common/MemoryRouterWrapped';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('./loginProvider', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+import loginProvider from './loginProvider';
+const mockLoginProvider = loginProvider as jest.Mock;
+
+describe('LoginButton', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouterWrapped
+        component={
+          <LoginButton
+            socialLoginRequestPath={'test-path'}
+            socialLoginRequestText={'test-text'}
+          />
+        }
+      />,
+    );
+  });
+
+  test('소셜 로그인 종류를 전달하면 해당 소셜 로그인 버튼이 렌더링된다.', () => {
+    const loginButton = screen.getByRole('button', { name: /test-text/i });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test('소셜 로그인 버튼을 누르면 해당 소셜 로그인 요청을 보낸다.', async () => {
+    const loginButton = screen.getByRole('button', { name: /test-text/i });
+    await userEvent.click(loginButton);
+    expect(mockLoginProvider).toHaveBeenCalledWith('test-path');
+  });
+});
