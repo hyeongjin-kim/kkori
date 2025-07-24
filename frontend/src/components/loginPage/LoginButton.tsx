b6e89cb1aac4a3bb11b3fbf3fe5f9094ff512f7c
hjkim <kims0887@naver.com>
2025-07-24 12:32:28 +0900

    refactor: 소셜 로그인에 필요한 정보들 타입 정리

--- frontend/src/components/loginPage/LoginButton.tsx
+++ frontend/src/components/loginPage/LoginButton.tsx
@@ -1,12 +1,10 @@
+import SocialLoginRequestProps from './SocialLoginRequestProps';
 import loginProvider from './loginProvider';
 
 function LoginButton({
   socialLoginRequestPath,
   socialLoginRequestText,
-}: {
-  socialLoginRequestPath: string;
-  socialLoginRequestText: string;
-}) {
+}: SocialLoginRequestProps) {
   return (
     <button
       className="w-full px-4 py-2 rounded border text-sm font-medium text-black bg-white hover:bg-gray-100 active:bg-gray-200"
