fdd1bfe49afef94e66be529ff35559e0d9397a4d
today-is-first <diligent0644@gmail.com>
2025-07-25 13:35:57 +0900

    refactor: BrowserRouter => MemoryRouter로 변경

--- frontend/src/components/homePage/PairPracticeButton.test.tsx
+++ frontend/src/components/homePage/PairPracticeButton.test.tsx
@@ -1,16 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import PairPracticeButton from '@components/homePage/PairPracticeButton';
-import BrowserRouterWrapped from '@components/common/BrowserRouterWrapped';
+import MemoryRouterWrapped from '@components/common/MemoryRouterWrapped';
 
-test('같이 연습하기 버튼이 렌더링 된다', () => {
-  render(<BrowserRouterWrapped component={<PairPracticeButton />} />);
-  expect(
-    screen.getByRole('button', { name: /같이 연습하기/i }),
-  ).toBeInTheDocument();
-});
-test('같이 연습하기 버튼을 클릭하면 같이 연습하기 페이지로 이동한다', () => {
-  render(<BrowserRouterWrapped component={<PairPracticeButton />} />);
-  const button = screen.getByRole('button', { name: /같이 연습하기/i });
-  fireEvent.click(button);
-  expect(window.location.pathname).toBe('/pair-practice');
+describe('PairPracticeButton', () => {
+  beforeEach(() => {
+    render(<MemoryRouterWrapped component={<PairPracticeButton />} />);
+  });
+  test('같이 연습하기 버튼이 렌더링 된다', () => {
+    expect(
+      screen.getByRole('button', { name: /같이 연습하기/i }),
+    ).toBeInTheDocument();
+  });
+
+  test('같이 연습하기 버튼을 클릭하면 같이 연습하기 페이지로 이동한다', () => {
+    const button = screen.getByRole('button', { name: /같이 연습하기/i });
+    fireEvent.click(button);
+    expect(
+      screen.getByRole('main', { name: 'pair-practice-page' }),
+    ).toBeInTheDocument();
+  });
 });
--- frontend/src/components/homePage/SoloPracticeButton.test.tsx
+++ frontend/src/components/homePage/SoloPracticeButton.test.tsx
@@ -1,17 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import SoloPracticeButton from '@components/homePage/SoloPracticeButton';
-import BrowserRouterWrapped from '@components/common/BrowserRouterWrapped';
+import MemoryRouterWrapped from '@components/common/MemoryRouterWrapped';
 
-test('혼자 연습하기 버튼이 렌더링 된다', () => {
-  render(<BrowserRouterWrapped component={<SoloPracticeButton />} />);
-  expect(
-    screen.getByRole('button', { name: /혼자 연습하기/i }),
-  ).toBeInTheDocument();
-});
+describe('SoloPracticeButton', () => {
+  beforeEach(() => {
+    render(<MemoryRouterWrapped component={<SoloPracticeButton />} />);
+  });
+  test('혼자 연습하기 버튼이 렌더링 된다', () => {
+    expect(
+      screen.getByRole('button', { name: /혼자 연습하기/i }),
+    ).toBeInTheDocument();
+  });
 
-test('혼자 연습하기 버튼을 클릭하면 혼자 연습하기 페이지로 이동한다', () => {
-  render(<BrowserRouterWrapped component={<SoloPracticeButton />} />);
-  const button = screen.getByRole('button', { name: /혼자 연습하기/i });
-  fireEvent.click(button);
-  expect(window.location.pathname).toBe('/solo-practice');
+  test('혼자 연습하기 버튼을 클릭하면 혼자 연습하기 페이지로 이동한다', () => {
+    const button = screen.getByRole('button', { name: /혼자 연습하기/i });
+    fireEvent.click(button);
+    expect(
+      screen.getByRole('main', { name: 'solo-practice-page' }),
+    ).toBeInTheDocument();
+  });
 });
