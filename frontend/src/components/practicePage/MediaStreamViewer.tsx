1a95ee355542888bc98f80aabd2c45c9d3334458
today-is-first <diligent0644@gmail.com>
2025-07-28 13:36:25 +0900

    feat: MediaStreamViewer 컴포넌트 구현

---
+++ frontend/src/components/practicePage/MediaStreamViewer.tsx
@@ -0,0 +1,44 @@
+import { MediaStreamType } from '@customTypes/practicePage/MediaStreamType';
+import useBindMediaStream from '@hooks/practicePage/useBindMediaStream';
+
+function MediaStreamViewer({ type }: MediaStreamType) {
+  const { videoRef, stream, isVideoOn, isAudioOn } = useBindMediaStream({
+    type,
+  });
+
+  return (
+    <div
+      aria-label="media-stream-viewer"
+      className="relative h-full w-full overflow-hidden rounded-xl bg-black"
+    >
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        className={`h-full w-full object-cover ${!isVideoOn ? 'opacity-0' : ''}`}
+      />
+
+      {!isVideoOn && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black text-white">
+          <img
+            src="/assets/video-off.png"
+            alt="video-off-image"
+            className="h-12 w-12"
+          />
+        </div>
+      )}
+
+      {!isAudioOn && (
+        <div className="absolute right-2 bottom-2 rounded-full bg-red-600 p-1 text-white">
+          <img
+            src="/assets/mic-off.png"
+            alt="mic-off-icon"
+            className="h-4 w-4"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MediaStreamViewer;
