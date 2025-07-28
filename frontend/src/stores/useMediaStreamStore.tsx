eade05d9c9f2e74bcb5aa5f5b8266723c6b6658c
today-is-first <diligent0644@gmail.com>
2025-07-28 13:35:31 +0900

    feat: useMediaStreamStore 구현

---
+++ frontend/src/stores/useMediaStreamStore.tsx
@@ -0,0 +1,42 @@
+import { create } from 'zustand';
+
+interface MediaStreamState {
+  myStream: MediaStream | null;
+  peerStream: MediaStream | null;
+  isMyVideoOn: boolean;
+  isMyAudioOn: boolean;
+  isPeerVideoOn: boolean;
+  isPeerAudioOn: boolean;
+}
+
+interface MediaStreamActions {
+  setMyStream: (myStream: MediaStream) => void;
+  setPeerStream: (peerStream: MediaStream) => void;
+  setIsMyVideoOn: (isMyVideoOn: boolean) => void;
+  setIsMyAudioOn: (isMyAudioOn: boolean) => void;
+  setIsPeerVideoOn: (isPeerVideoOn: boolean) => void;
+  setIsPeerAudioOn: (isPeerAudioOn: boolean) => void;
+}
+
+const initialState: MediaStreamState = {
+  myStream: null,
+  peerStream: null,
+  isMyVideoOn: false,
+  isMyAudioOn: false,
+  isPeerVideoOn: false,
+  isPeerAudioOn: false,
+};
+
+const useMediaStreamStore = create<MediaStreamState & MediaStreamActions>(
+  set => ({
+    ...initialState,
+    setMyStream: myStream => set({ myStream }),
+    setPeerStream: peerStream => set({ peerStream }),
+    setIsMyVideoOn: isMyVideoOn => set({ isMyVideoOn }),
+    setIsMyAudioOn: isMyAudioOn => set({ isMyAudioOn }),
+    setIsPeerVideoOn: isPeerVideoOn => set({ isPeerVideoOn }),
+    setIsPeerAudioOn: isPeerAudioOn => set({ isPeerAudioOn }),
+  }),
+);
+
+export default useMediaStreamStore;
