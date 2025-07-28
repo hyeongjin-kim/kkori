42e86c4169339002f5a04e56fee6685abe49ba60
today-is-first <diligent0644@gmail.com>
2025-07-28 13:36:54 +0900

    feat: PracticePage에 컴포넌트 추가

--- frontend/src/pages/PracticePage/index.tsx
+++ frontend/src/pages/PracticePage/index.tsx
@@ -1,5 +1,14 @@
+import CurrentQuestionDisplay from '@/components/practicePage/CurrentQuestionDisplay';
+import MediaStreamViewer from '@/components/practicePage/MediaStreamViewer';
+
 function PracticePage() {
-  return <main aria-label="practice-page">PracticePage</main>;
+  return (
+    <main aria-label="practice-page">
+      <CurrentQuestionDisplay id={1} question="현재 질문" />
+      <MediaStreamViewer type="my" />
+      <MediaStreamViewer type="peer" />
+    </main>
+  );
 }
 
 export default PracticePage;
