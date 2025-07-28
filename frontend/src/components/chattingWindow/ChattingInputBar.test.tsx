8864bdec243a380fc48328c1c749e55c84e33100
hjkim <kims0887@naver.com>
2025-07-28 17:47:22 +0900

    test: 채팅 입력 바 테스트 구현

---
+++ frontend/src/components/chattingWindow/ChattingInputBar.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+jest.mock('./SubmitChatting');
+import userEvent from '@testing-library/user-event';
+import ChattingInputBar from '@/components/chattingWindow/ChattingInputBar';
+import SubmitChatting from '@/components/chattingWindow/SubmitChatting';
+const mockSubmitChatting = SubmitChatting as jest.Mock;
+
+describe('ChattingInputBar', () => {
+  beforeEach(() => {
+    render(<ChattingInputBar />);
+  });
+
+  test('채팅 입력창이 랜더링 되어야 한다.', () => {
+    expect(
+      screen.getByRole('textbox', { name: 'chatting-input' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('엔터를 누르면 채팅을 제출하는 함수가 실행되어야 한다.', async () => {
+    const textInput = screen.getByRole('textbox');
+    await userEvent.type(textInput, 'test');
+    await userEvent.keyboard('{Enter}');
+    expect(mockSubmitChatting).toHaveBeenCalledWith(expect.any(Object));
+  });
+  test('버튼을 누르면 채팅을 제출하는 함수가 실행되어야 한다.', async () => {
+    const sendButton = screen.getByRole('button');
+    await userEvent.click(sendButton);
+    expect(mockSubmitChatting).toHaveBeenCalledWith(expect.any(Object));
+  });
+});
