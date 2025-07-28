033885d422cb87d245956f2f442bcf8c465836a8
today-is-first <diligent0644@gmail.com>
2025-07-28 12:12:53 +0900

    chore: 절대 경로 수정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -13,7 +13,7 @@ export default {
     '@customTypes/(.*)': '<rootDir>/src/customTypes/$1',
     '@layouts/(.*)': '<rootDir>/src/layouts/$1',
     '@pages/(.*)': '<rootDir>/src/pages/$1',
-    '@store/(.*)': '<rootDir>/src/stores/$1',
+    '@stores/(.*)': '<rootDir>/src/stores/$1',
     '@utils/(.*)': '<rootDir>/src/utils/$1',
     '@hooks/(.*)': '<rootDir>/src/hooks/$1',
   },
