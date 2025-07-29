ae9e0c201efb29132e1d48950bd17ae34f93fb3f
today-is-first <diligent0644@gmail.com>
2025-07-29 16:14:09 +0900

    chore: 주석 제거

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,9 +1,6 @@
-//메인 레이아웃에 있어야 할 것들
-
 import { Outlet } from 'react-router-dom';
 import Header from '@components/common/Header';
 
-// 1. 헤더
 function MainLayout() {
   return (
     <div className="bg-background flex h-full min-h-screen flex-col">
