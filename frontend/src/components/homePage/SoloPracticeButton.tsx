06dcb4a78186c70274f1bbd75544039153cd7217
today-is-first <diligent0644@gmail.com>
2025-07-29 16:00:02 +0900

    feat: Practice 버튼 스타일 주입할 수 있는 기능 추가

--- frontend/src/components/homePage/SoloPracticeButton.tsx
+++ frontend/src/components/homePage/SoloPracticeButton.tsx
@@ -1,6 +1,12 @@
 import PracticeButton from '@components/homePage/PracticeButton';
 function SoloPracticeButton() {
-  return <PracticeButton text="혼자 연습하기" path="/solo-practice" />;
+  return (
+    <PracticeButton
+      text="혼자 연습하기"
+      path="/solo-practice"
+      className="bg-point-500 text-text-black"
+    />
+  );
 }
 
 export default SoloPracticeButton;
