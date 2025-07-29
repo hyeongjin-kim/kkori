011354e055241eb2fb84f8ece623538b3f2d76b9
today-is-first <diligent0644@gmail.com>
2025-07-29 16:14:17 +0900

    style: css 수정

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -5,7 +5,7 @@ import GoToLoginButton from '@components/header/GoToLoginButton';
 
 function Header() {
   return (
-    <header className="text-text-white border-underline-gray flex h-16 items-center justify-between border-b px-32">
+    <header className="text-text-white border-underline-gray flex min-h-16 items-center justify-between border-b px-32">
       <Logo textClassName="text-2xl" imgClassName="h-8 w-8 -ml-3 mt-1" />
       <NavigationBar />
       <GoToLoginButton />
