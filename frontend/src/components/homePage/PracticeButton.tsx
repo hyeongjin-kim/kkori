fda5afdbc854f1c0934943470e74fb4256a8c950
hjkim <kims0887@naver.com>
2025-07-31 14:49:31 +0900

    feat: 혼자연습하기 버튼에 방 생성 기능 구현

--- frontend/src/components/homePage/PracticeButton.tsx
+++ frontend/src/components/homePage/PracticeButton.tsx
@@ -1,5 +1,7 @@
 // PracticeButton.tsx
 import { useNavigate } from 'react-router-dom';
+import { useWebSocketStore } from '@/stores/useWebSocketStore';
+import { RoomCreateRequest } from '@/customTypes/practicePage/RoomCreateRequest';
 
 interface PracticeButtonProps {
   text: string;
@@ -13,10 +15,16 @@ export default function PracticeButton({
   className,
 }: PracticeButtonProps) {
   const navigate = useNavigate();
-
+  const roomCreate = useWebSocketStore(state => state.roomCreate);
   return (
     <button
-      onClick={() => navigate(path)}
+      onClick={() => {
+        roomCreate({
+          mode: 'SOLO_PRACTICE',
+          questionSetId: 1,
+        });
+        navigate(path);
+      }}
       className={`hover:bg-hover-gray bg-background flex h-[56px] max-w-[320px] min-w-[140px] items-center justify-center rounded-lg px-8 py-6 shadow-sm transition ${className} `}
     >
       <span className="text-lg font-semibold">{text}</span>
