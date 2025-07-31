ad487e500771030ff648b6b57136eaf6ea86f07f
hjkim <kims0887@naver.com>
2025-07-31 14:49:09 +0900

    feat: 방 연결 요청 로직 구현

--- frontend/src/stores/useWebSocketStore.tsx
+++ frontend/src/stores/useWebSocketStore.tsx
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
+import { RoomCreateRequest } from '@/customTypes/practicePage/RoomCreateRequest';
 
 interface WebSocketState {
   client: Client | null;
@@ -10,6 +11,15 @@ interface WebSocketState {
 interface WebSocketAction {
   connect: () => void;
   disconnect: () => void;
+  roomCreate: (request: RoomCreateRequest) => void;
+  interviewStart: () => void;
+  interviewEnd: () => void;
+  roomJoin: () => void;
+  roomExit: () => void;
+  answerStart: () => void;
+  answerSubmit: () => void;
+  nextQuestionSelect: () => void;
+  currentQuestionStart: () => void;
 }
 
 const initialState: WebSocketState = {
@@ -32,6 +42,11 @@ export const useWebSocketStore = create<WebSocketState & WebSocketAction>(
         heartbeatOutgoing: 4000,
         onConnect: () => {
           set({ client, isConnected: true });
+          client.subscribe('/user/queue/interview', message => {
+            const response = JSON.parse(message.body);
+            console.log(response);
+            personalMessageHandler(response);
+          });
         },
         onDisconnect: () => {
           set({ client: null, isConnected: false });
@@ -43,5 +58,45 @@ export const useWebSocketStore = create<WebSocketState & WebSocketAction>(
       if (!get().client || !get().isConnected) return;
       get().client?.deactivate();
     },
+    roomCreate: (request: RoomCreateRequest) => {
+      get().client?.publish({
+        destination: '/app/room/create',
+        body: JSON.stringify(request),
+      });
+    },
+    interviewStart: () => {},
+    interviewEnd: () => {},
+    roomJoin: () => {},
+    roomExit: () => {},
+    answerStart: () => {},
+    answerSubmit: () => {},
+    nextQuestionSelect: () => {},
+    currentQuestionStart: () => {},
+    currentQuestionCreate: () => {},
   }),
 );
+
+const personalMessageHandler = (response: any) => {
+  switch (response.type) {
+    case 'room-created':
+      roomCreatedHandler(response.data);
+      break;
+    case 'room-status':
+      roomStatusHandler(response.data);
+      break;
+    case 'answer-processed':
+      answerProcessedHandler(response.data);
+      break;
+    case 'error':
+      errorHandler(response.data);
+      break;
+  }
+};
+
+const roomCreatedHandler = (response: any) => {};
+
+const roomStatusHandler = (response: any) => {};
+
+const answerProcessedHandler = (response: any) => {};
+
+const errorHandler = (response: any) => {};
