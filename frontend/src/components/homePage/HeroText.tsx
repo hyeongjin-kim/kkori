0f0daa295a31b30c80ecc49508fc456e23b64376
hjkim <kims0887@naver.com>
2025-08-01 15:41:13 +0900

    refactor: 로고 단일 이미지로 대체

--- frontend/src/components/homePage/HeroText.tsx
+++ frontend/src/components/homePage/HeroText.tsx
@@ -9,7 +9,7 @@ function HeroText() {
       <p>꼬리에 꼬리를 무는 면접 질문</p>
       <div className="flex items-center">
         <p className="mr-2">이제는</p>
-        <Logo textClassName="text-4xl" imgClassName="h-10 w-10 -ml-4 mt-2" />
+        <Logo className="mt-2 -ml-4 h-10 w-10" />
         <p>와 함께 AI로 연습하세요</p>
       </div>
     </div>
