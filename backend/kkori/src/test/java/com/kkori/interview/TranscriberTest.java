f02c3dda5fbca318c582c323b52412ca49948617
SwnBae <128581113+SwnBae@users.noreply.github.com>
2025-08-03 22:05:23 +0900

    feat: webm, Base64 오디오 처리 테스트 추가

--- backend/kkori/src/test/java/com/kkori/interview/TranscriberTest.java
+++ backend/kkori/src/test/java/com/kkori/interview/TranscriberTest.java
@@ -15,6 +15,9 @@ public class TranscriberTest {
     @Autowired
     private Transcriber transcriber;
 
+    /**
+     * 임시로 로컬에 있는 m4a를 사용합니다.
+     */
     @Test
     @DisplayName("생성된 오디오 파일을 API를 통해 텍스트로 변환해야 한다")
     void should_transcribe_audio_file_through_API() {
