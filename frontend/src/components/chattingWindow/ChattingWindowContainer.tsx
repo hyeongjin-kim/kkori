6bf0282419a00d1d99336a79b057fa39758be3ef
hjkim <kims0887@naver.com>
2025-08-04 16:31:32 +0900

    refactor: 래퍼 div 컴포넌트로 분리

--- frontend/src/components/chattingWindow/ChattingWindowContainer.tsx
+++ frontend/src/components/chattingWindow/ChattingWindowContainer.tsx
@@ -9,10 +9,7 @@ function ChattingWindowContainer() {
       className="flex h-[calc(100vh-8rem)] max-h-[calc(100vh-8rem)] w-1/4 flex-col overflow-hidden rounded-2xl border border-gray-200 bg-white py-8 shadow-md"
     >
       <ScrollableList />
-
-      <div className="border-t border-gray-200 bg-white px-4 py-3">
-        <ChattingInputBar />
-      </div>
+      <ChattingInputBar />
     </div>
   );
 }
