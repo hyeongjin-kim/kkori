c8f395bc4068525b094221484fd567b811b9bae4
hjkim <kims0887@naver.com>
2025-08-04 13:07:17 +0900

    chore: 질문 초기 상태 테스트용 질문으로 설정

--- frontend/src/stores/useCurrentQuestionStore.tsx
+++ frontend/src/stores/useCurrentQuestionStore.tsx
@@ -16,7 +16,10 @@ interface CurrentQuestionActions {
 type CurrentQuestionStore = CurrentQuestionState & CurrentQuestionActions;
 
 const initialState: CurrentQuestionState = {
-  question: { id: 0, question: '' },
+  question: {
+    id: 0,
+    question: '명령형 프로그래밍과 선언형 프로그래밍이 무엇인가요?',
+  },
 };
 
 export const useCurrentQuestionStore = create<CurrentQuestionStore>(set => ({
