edf0c42c2615791b47d6a8148533a838bd1efcd4
hjkim <kims0887@naver.com>
2025-08-04 17:09:16 +0900

    feat: 면접 시작 및 끝내기 함수 추가

--- frontend/src/components/practicePage/controlButtonfunction.ts
+++ frontend/src/components/practicePage/controlButtonfunction.ts
@@ -9,3 +9,11 @@ export function startAnswer() {
 export function endAnswer() {
   console.log('endAnswer');
 }
+
+export function endInterview() {
+  console.log('endInterview');
+}
+
+export function startInterview() {
+  console.log('startInterview');
+}
