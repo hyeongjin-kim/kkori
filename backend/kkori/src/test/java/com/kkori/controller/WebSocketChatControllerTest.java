0685e2246104d2d6e18bbcba22fc9e968492df7a
jin_0410 <jin_0410@naver.com>
2025-08-05 16:57:14 +0900

    test: 잘못된 방에 메시지 전송 테스트 추가

--- backend/kkori/src/test/java/com/kkori/controller/WebSocketChatControllerTest.java
+++ backend/kkori/src/test/java/com/kkori/controller/WebSocketChatControllerTest.java
@@ -1,13 +1,20 @@
 package com.kkori.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kkori.dto.interview.request.CommonRoomRequest;
 import com.kkori.dto.interview.request.RoomCreateRequest;
 import com.kkori.dto.interview.response.RoomCreateResponse;
 import com.kkori.dto.websocket.WebSocketChatMessage;
 import com.kkori.entity.User;
+import com.kkori.exception.interview.InterviewRoomException;
 import com.kkori.jwt.Token;
 import com.kkori.jwt.TokenProvider;
 import com.kkori.service.InterviewSessionService;
@@ -46,10 +53,12 @@ public class WebSocketChatControllerTest {
 
     private final Long TEST_USER_ID_1 = 123L;
     private final Long TEST_USER_ID_2 = 456L;
-    private final String TEST_ROOM_ID = "REAL_ROOM_456";
+    private final String TEST_ROOM_ID_1 = "REAL_ROOM_456";
+    private final String TEST_ROOM_ID_2 = "REAL_ROOM_789";
     private String jwt_user1, jwt_user2;
     private User user1, user2;
     private final String TEST_CHAT_MESSAGE = "테스트 메시지123abc";
+    private final String TEST_CHAT_MESSAGE_FAIL = "실패할 테스트 메시지123abc";
 
     @BeforeEach
     void setUp() throws Exception {
@@ -115,7 +124,8 @@ public class WebSocketChatControllerTest {
         // given
 
         given(interviewSessionService.createPairRoom(1L, TEST_USER_ID_2))
-                .willReturn(TEST_ROOM_ID);
+                .willReturn(TEST_ROOM_ID_1);
+        doNothing().when(interviewSessionService).canSendChatMessage(any(), any());
 
         RoomCreateRequest roomCreateRequest = new RoomCreateRequest("PAIR_INTERVIEW", 1l);
         stompSession_user2.send("/app/room-create", roomCreateRequest);
@@ -125,6 +135,9 @@ public class WebSocketChatControllerTest {
         Map<String, Object> responsePayload = (Map<String, Object>) response.get("data");
         RoomCreateResponse roomCreateResponse = objectMapper.convertValue(responsePayload, RoomCreateResponse.class);
         String roomId = roomCreateResponse.getRoomId();
+
+        CommonRoomRequest roomJoinRequest = new CommonRoomRequest(roomId);
+        stompSession_user2.send("/app/room-join", roomJoinRequest);
 
         WebSocketTestHelper.MessageSubscriber chatSubscriber_user1 = testHelper.subscribeToRealRoomTopic(
                 stompSession_user1, roomId);
@@ -145,6 +158,64 @@ public class WebSocketChatControllerTest {
         WebSocketChatMessage chatMessage_user2 = objectMapper.convertValue(chatResponse_user2.get("data"),
                 WebSocketChatMessage.class);
         assertThat(chatMessage_user1.content()).isEqualTo(TEST_CHAT_MESSAGE);
+        assertThat(chatMessage_user2.content()).isEqualTo(TEST_CHAT_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("자신이 포함되지 않은 방에 채팅을 보내면 해당 방의 유저는 아무것도 받지 않고, 자신은 에러 메시지를 받는다")
+    void sendChatToWrongRoom() throws Exception {
+        // given
+        given(interviewSessionService.createPairRoom(1L, TEST_USER_ID_1))
+                .willReturn(TEST_ROOM_ID_1);
+        given(interviewSessionService.createPairRoom(1L, TEST_USER_ID_2))
+                .willReturn(TEST_ROOM_ID_2);
+        doThrow(InterviewRoomException.userNotFoundInRoom()).when(interviewSessionService)
+                .canSendChatMessage(eq(TEST_ROOM_ID_2), eq(TEST_USER_ID_1));
+
+        RoomCreateRequest roomCreateRequest = new RoomCreateRequest("PAIR_INTERVIEW", 1l);
+        stompSession_user1.send("/app/room-create", roomCreateRequest);
+
+        Map<String, Object> response = personalSubscriber_user1.waitForMessage("room-created", 3);
+
+        Map<String, Object> responsePayload = (Map<String, Object>) response.get("data");
+        RoomCreateResponse roomCreateResponse = objectMapper.convertValue(responsePayload, RoomCreateResponse.class);
+        String roomId_1 = roomCreateResponse.getRoomId();
+
+//        CommonRoomRequest roomJoinRequest = new CommonRoomRequest(roomId);
+//        stompSession_user2.send("/app/room-join", roomJoinRequest);
+
+        RoomCreateRequest roomCreateRequest2 = new RoomCreateRequest("PAIR_INTERVIEW", 1l);
+        stompSession_user2.send("/app/room-create", roomCreateRequest2);
+
+        Map<String, Object> response2 = personalSubscriber_user2.waitForMessage("room-created", 3);
+
+        Map<String, Object> responsePayload2 = (Map<String, Object>) response2.get("data");
+        RoomCreateResponse roomCreateResponse2 = objectMapper.convertValue(responsePayload2, RoomCreateResponse.class);
+        String roomId_2 = roomCreateResponse2.getRoomId();
+
+        WebSocketTestHelper.MessageSubscriber chatSubscriber_user1 = testHelper.subscribeToRealRoomTopic(
+                stompSession_user1, roomId_1);
+        WebSocketTestHelper.MessageSubscriber chatSubscriber_user2 = testHelper.subscribeToRealRoomTopic(
+                stompSession_user2, roomId_2);
+
+        // when
+        WebSocketChatMessage chatMessageSend = new WebSocketChatMessage(
+                roomId_2, user1.getNickname(), TEST_CHAT_MESSAGE_FAIL, System.currentTimeMillis()
+        );
+        stompSession_user1.send("/app/chat", chatMessageSend);
+
+        WebSocketChatMessage chatMessageSend2 = new WebSocketChatMessage(
+                roomId_2, user2.getNickname(), TEST_CHAT_MESSAGE, System.currentTimeMillis()
+        );
+        stompSession_user2.send("/app/chat", chatMessageSend2);
+
+        // then
+        assertThrows(AssertionError.class, () -> chatSubscriber_user1.waitForMessage("chat", 3));
+        Map<String, Object> errorResponse = personalSubscriber_user1.waitForMessage("error", 3);
+        Map<String, Object> chatResponse_user2 = chatSubscriber_user2.waitForMessage("chat", 3);
+        assertThrows(AssertionError.class, () -> chatSubscriber_user2.waitForMessage("chat", 3));
+        WebSocketChatMessage chatMessage_user2 = objectMapper.convertValue(chatResponse_user2.get("data"),
+                WebSocketChatMessage.class);
         assertThat(chatMessage_user2.content()).isEqualTo(TEST_CHAT_MESSAGE);
     }
 }
