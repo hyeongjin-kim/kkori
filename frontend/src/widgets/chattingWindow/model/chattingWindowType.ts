7668c7a90c096dbfdd5b8375d6ff8bd2aa1d6a01
today-is-first <diligent0644@gmail.com>
2025-08-06 12:25:18 +0900

    feat: Message 타입 추가

---
+++ frontend/src/widgets/chattingWindow/model/chattingWindowType.ts
@@ -0,0 +1,18 @@
+export const CHAT_TYPES = {
+  QUESTION: 'question',
+  ANSWER: 'answer',
+  CHAT: 'chat',
+} as const;
+
+export interface NameTaggedMessageProps {
+  message: Message;
+}
+
+export interface Message {
+  id: string;
+  type: (typeof CHAT_TYPES)[keyof typeof CHAT_TYPES];
+  sender: string;
+  text: string;
+  timestamp: string;
+  isMyMessage?: boolean;
+}
--- frontend/src/widgets/chattingWindow/model/index.ts
+++ frontend/src/widgets/chattingWindow/model/index.ts
@@ -1,5 +1,6 @@
 import useChattingWindowStore from '@/stores/useChattingWindowStore';
 import { FormEvent } from 'react';
+import { CHAT_TYPES } from '@/widgets/chattingWindow/model/chattingWindowType';
 
 export const submitChatting = (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault();
@@ -11,8 +12,10 @@ export const submitChatting = (e: FormEvent<HTMLFormElement>) => {
   //TODO: 스크롤 최하단으로 유지하는 기능 추가
   useChattingWindowStore.getState().addMessage({
     id,
-    type: 'user',
-    message: text,
+    type: CHAT_TYPES.CHAT,
+    text,
     sender: 'me', // TODO: 유저 이름 받아오기
+    timestamp: new Date().toISOString(),
+    isMyMessage: true,
   });
 };
