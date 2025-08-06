7835f9220ccf3ba3d116485f40891a40fc4f5e3b
today-is-first <diligent0644@gmail.com>
2025-08-06 15:57:36 +0900

    chore: 태그 필터 리스트 상수화

---
+++ frontend/src/pages/interviewQuestionsPage/model/constants.ts
@@ -0,0 +1,32 @@
+import { Tag } from '@/entities/questionSet/model/type';
+
+export const TAG_FILTER_LIST: Tag[] = Object.freeze([
+  {
+    id: 1,
+    tag: '면접',
+  },
+  {
+    id: 2,
+    tag: '백엔드',
+  },
+  {
+    id: 3,
+    tag: '프론트엔드',
+  },
+  {
+    id: 4,
+    tag: '데이터베이스',
+  },
+  {
+    id: 5,
+    tag: '네트워크',
+  },
+  {
+    id: 6,
+    tag: '운영체제',
+  },
+  {
+    id: 7,
+    tag: '알고리즘',
+  },
+]);
