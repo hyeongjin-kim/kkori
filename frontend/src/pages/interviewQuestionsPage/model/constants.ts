e124c5fd64a3ea4356ae7b67f2bcef5d18ed5fe9
today-is-first <diligent0644@gmail.com>
2025-08-06 17:26:38 +0900

    chore: 전체 태그 추가

--- frontend/src/pages/interviewQuestionsPage/model/constants.ts
+++ frontend/src/pages/interviewQuestionsPage/model/constants.ts
@@ -1,6 +1,10 @@
 import { Tag } from '@/entities/questionSet/model/type';
 
-export const TAG_FILTER_LIST: Tag[] = Object.freeze([
+export const TAG_FILTER_LIST: readonly Tag[] = Object.freeze([
+  {
+    id: 0,
+    tag: '전체',
+  },
   {
     id: 1,
     tag: '면접',
@@ -28,5 +32,29 @@ export const TAG_FILTER_LIST: Tag[] = Object.freeze([
   {
     id: 7,
     tag: '알고리즘',
+  },
+  {
+    id: 8,
+    tag: '자료구조',
+  },
+  {
+    id: 9,
+    tag: 'Java',
+  },
+  {
+    id: 10,
+    tag: 'Git',
+  },
+  {
+    id: 11,
+    tag: 'CSS',
+  },
+  {
+    id: 12,
+    tag: 'React',
+  },
+  {
+    id: 13,
+    tag: '웹 성능',
   },
 ]);
