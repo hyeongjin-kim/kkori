22aef96af2fbcf99b46dfb631e07dffdac8d32b6
hjkim <kims0887@naver.com>
2025-08-07 13:12:54 +0900

    refactor: 웹소켓 스토어 분할하여 슬라이스로 리펙토링

---
+++ frontend/src/shared/lib/webSocketSlice.tsx
@@ -0,0 +1,265 @@
+import { StateCreator } from 'zustand';
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
+import { InterviewSlice } from '@/widgets/interviewSection/model/interviewSlice';
+import { ChattingWindowSlice } from '@/widgets/chattingWindow/model/chattingWindowSlice';
+import { PRACTICE_MODE } from '@/pages/homePage/ui/PracticeButton';
+import useMediaStreamStore from '@/widgets/interviewSection/model/useMediaStreamStore';
+
+interface RoomCreateRequest {
+  mode: string;
+  questionSetId: number;
+}
+interface WebSocketState {
+  client: Client | null;
+  isConnected: boolean;
+  roomID: string | null;
+  practiceMode: (typeof PRACTICE_MODE)[keyof typeof PRACTICE_MODE];
+  questionSetId: number;
+}
+type Store = ChattingWindowSlice & WebSocketSlice & InterviewSlice;
+
+interface WebSocketAction {
+  connect: (
+    practiceMode: (typeof PRACTICE_MODE)[keyof typeof PRACTICE_MODE],
+    questionSetId: number,
+  ) => void;
+  disconnect: () => void;
+  roomCreate: (request: RoomCreateRequest) => void;
+  interviewStart: (roomID: string) => void;
+  interviewEnd: (roomID: string) => void;
+  roomJoin: (roomID: string) => void;
+  roomExit: (roomID: string) => void;
+  answerStart: (roomID: string, questionID: string) => void;
+  answerSubmit: (roomID: string, questionID: string) => void;
+  nextQuestionSelect: (roomID: string) => void;
+  customQuestionStart: (roomID: string) => void;
+  customQuestionCreate: (roomID: string, questionText: string) => void;
+}
+
+export interface WebSocketSlice extends WebSocketState, WebSocketAction {}
+
+const initialState: WebSocketState = {
+  client: null,
+  isConnected: false,
+  roomID: null,
+  practiceMode: PRACTICE_MODE.SOLO_PRACTICE,
+  questionSetId: 0,
+};
+
+export const createWebSocketSlice: StateCreator<
+  Store,
+  [],
+  [],
+  WebSocketSlice
+> = (set, get) => ({
+  ...initialState,
+  connect: (
+    practiceMode: (typeof PRACTICE_MODE)[keyof typeof PRACTICE_MODE],
+    questionSetId: number,
+  ) => {
+    if (get().client || get().isConnected) return;
+    const client = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+      debug: str => {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: () => {
+        set({ client, isConnected: true });
+        client.subscribe('/user/queue/interview', message => {
+          const response = JSON.parse(message.body);
+          console.log(response);
+          personalMessageHandler(client, get, set, response);
+        });
+        get().roomCreate({ mode: practiceMode, questionSetId: questionSetId });
+      },
+      onDisconnect: () => {
+        set({ client: null, isConnected: false });
+      },
+    });
+    client.activate();
+  },
+  disconnect: () => {
+    if (!get().client || !get().isConnected) return;
+    get().client?.deactivate();
+  },
+  roomCreate: (request: RoomCreateRequest) => {
+    get().client?.publish({
+      destination: '/app/room-create',
+      body: JSON.stringify(request),
+    });
+  },
+  interviewStart: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/interview-start`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  interviewEnd: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/interview-end`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  roomJoin: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/room-join`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  roomExit: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/room-exit`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  sendMessage: (roomID: string, sender: string, text: string) => {
+    get().client?.publish({
+      destination: `/app/chat`,
+      body: JSON.stringify({ roomId: roomID, sender: sender, text: text }),
+    });
+  },
+  answerStart: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/answer-start`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  answerSubmit: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/answer-submit`,
+      body: JSON.stringify({
+        roomId: roomID,
+        //TODO: 오디오 파일 같이 전달해야함
+      }),
+    });
+  },
+  nextQuestionSelect: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/next-question-select`,
+      body: JSON.stringify({
+        roomId: roomID,
+        questionType: 'DEFAULT',
+        questionId: '1',
+        questionText: 'test',
+      }),
+    });
+  },
+  customQuestionStart: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/custom-question-start`,
+      body: JSON.stringify({ roomId: roomID }),
+    });
+  },
+  customQuestionCreate: (roomID: string) => {
+    get().client?.publish({
+      destination: `/app/custom-question-create`,
+      body: JSON.stringify({
+        roomId: roomID,
+        //TODO: 커스텀 질문 오디오 파일 전달해야 함
+      }),
+    });
+  },
+});
+
+const personalMessageHandler = (
+  client: Client,
+  get: any,
+  set: any,
+  response: any,
+) => {
+  switch (response.type) {
+    case 'room-created':
+      roomCreatedHandler(client, get, set, response.data);
+      break;
+    case 'existing-user':
+      existingUserHandler(client, set, response.data);
+      break;
+    case 'joined-user':
+      joinedUserHandler(client, set, response.data);
+
+      break;
+    case 'room-status':
+      roomStatusHandler(client, set, response.data);
+      break;
+    case 'offer':
+      offerHandler(client, set, response.data);
+      break;
+    case 'answer':
+      //RTC 응답
+      answerHandler(client, set, response.data);
+      break;
+    default:
+      errorHandler(client, set, response.data);
+      break;
+  }
+};
+
+const roomCreatedHandler = (
+  client: Client,
+  get: any,
+  set: any,
+  response: any,
+) => {
+  set({ roomID: response.roomId });
+  client.subscribe(`/topic/interview/${response.roomId}`, message => {
+    const response = JSON.parse(message.body);
+    get().InterviewMessageHandler(client, response);
+  });
+};
+
+const existingUserHandler = async (client: Client, set: any, response: any) => {
+  const peerConnection = new RTCPeerConnection();
+  const myStream = useMediaStreamStore.getState().myStream;
+  if (myStream) {
+    myStream.getTracks().forEach(track => {
+      peerConnection.addTrack(track, myStream);
+    });
+  }
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  client.publish({
+    destination: `/app/offer`,
+    body: JSON.stringify({
+      roomId: response.roomId,
+      offer: offer,
+    }),
+  });
+};
+
+const joinedUserHandler = (client: Client, set: any, response: any) => {};
+
+const roomStatusHandler = (client: Client, set: any, response: any) => {
+  console.log(response);
+};
+
+const offerHandler = async (client: Client, set: any, response: any) => {
+  const peerConnection = new RTCPeerConnection();
+  const myStream = useMediaStreamStore.getState().myStream;
+  if (myStream) {
+    myStream.getTracks().forEach(track => {
+      peerConnection.addTrack(track, myStream);
+    });
+  }
+  peerConnection.setRemoteDescription(response.offer);
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+  client.publish({
+    destination: `/app/answer`,
+    body: JSON.stringify({
+      roomId: response.roomId,
+      answer: answer,
+    }),
+  });
+};
+
+const answerHandler = (client: Client, set: any, response: any) => {
+  console.log(response);
+};
+
+const errorHandler = (client: Client, set: any, response: any) => {
+  console.log(response);
+};
