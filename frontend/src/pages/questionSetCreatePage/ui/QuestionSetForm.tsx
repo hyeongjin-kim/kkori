d320d17e12dc9617b8dff158b4908705b124863a
today-is-first <diligent0644@gmail.com>
2025-08-07 15:35:45 +0900

    feat: QuestionSetForm 태그 기능 추가

--- frontend/src/pages/questionSetCreatePage/ui/QuestionSetForm.tsx
+++ frontend/src/pages/questionSetCreatePage/ui/QuestionSetForm.tsx
@@ -2,12 +2,15 @@ import TitleInput from '@/entities/questionSet/ui/TitleInput';
 import DescriptionInput from '@/entities/questionSet/ui/DescriptionInput';
 import SharedToggleSwitch from '@/entities/questionSet/ui/SharedToggleSwitch';
 import { useState } from 'react';
+import TagListInput from '@/entities/questionSet/ui/TagListInput';
+import TagDisplay from '@/entities/questionSet/ui/TagDisplay';
 
 function QuestionSetForm() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [isShared, setIsShared] = useState(false);
-  const [tagList, setTagList] = useState<string[]>([]);
+  const [tagInput, setTagInput] = useState('');
+  const [tagList, setTagList] = useState<Set<string>>(new Set());
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -21,6 +24,23 @@ function QuestionSetForm() {
     setIsShared(value);
   };
 
+  const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTagInput(e.target.value);
+  };
+
+  const handleTagSubmit = () => {
+    setTagList(prev => new Set([...prev, tagInput]));
+    setTagInput('');
+  };
+
+  const handleTagClick = (tag: string) => {
+    setTagList(prev => {
+      const newSet = new Set(prev);
+      newSet.delete(tag);
+      return newSet;
+    });
+  };
+
   return (
     <section
       aria-label="question-set-form"
@@ -29,33 +49,27 @@ function QuestionSetForm() {
       <h3 className="mb-6 text-xl font-bold text-gray-900">질문 세트 생성</h3>
       <form className="flex flex-col gap-6">
         <TitleInput
-          displayTitle="면접 질문 세트 제목"
+          displayTitle="제목"
           value={title}
           onChange={handleTitleChange}
         />
         <DescriptionInput
-          displayTitle="면접 질문 세트 설명"
+          displayTitle="설명"
           value={description}
           onChange={handleDescriptionChange}
         />
         <SharedToggleSwitch
-          displayTitle="면접 질문 세트 공개 여부"
+          displayTitle="공개 여부"
           value={isShared}
           onChange={handleIsSharedChange}
         />
-
-        <div className="flex flex-col gap-1">
-          <label htmlFor="tag" className="text-sm font-medium text-gray-500">
-            태그
-          </label>
-          <input
-            type="text"
-            id="tag"
-            aria-label="tag-input"
-            className="focus:border-point-400 focus:ring-point-400 rounded-lg border border-gray-200 bg-gray-50 px-4 py-2 text-sm text-gray-900 placeholder-gray-400 focus:ring-1 focus:outline-none"
-            placeholder="#태그를 입력하세요"
-          />
-        </div>
+        <TagListInput
+          displayTitle="태그"
+          value={tagInput}
+          onChange={handleTagChange}
+          onSubmit={handleTagSubmit}
+        />
+        <TagDisplay tags={Array.from(tagList)} onClick={handleTagClick} />
       </form>
     </section>
   );
