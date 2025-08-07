67a7aeec546201b8d2648b96da9a4554eb13b14c
today-is-first <diligent0644@gmail.com>
2025-08-07 10:57:24 +0900

    test: TagInputForm handleSubmit 이벤트 테스트 추가

--- frontend/src/pages/interviewQuestionsPage/test/TagInputForm.test.tsx
+++ frontend/src/pages/interviewQuestionsPage/test/TagInputForm.test.tsx
@@ -1,11 +1,37 @@
-import { render, screen } from '@testing-library/react';
+jest.mock('@/pages/interviewQuestionsPage/model/useQuestionSetFilterStore');
+
+import { fireEvent, render, screen } from '@testing-library/react';
 import TagInputForm from '@/pages/interviewQuestionsPage/ui/TagInputForm';
+import userEvent from '@testing-library/user-event';
+import useQuestionSetFilterStore from '@/pages/interviewQuestionsPage/model/useQuestionSetFilterStore';
 
 describe('TagInputForm', () => {
-  test('TagInputForm 컴포넌트가 렌더링되어야 합니다.', () => {
+  const setSelectedTagMock = jest.fn();
+  beforeEach(() => {
+    const mockedUseQuestionSetFilterStore =
+      useQuestionSetFilterStore as unknown as jest.Mock;
+    mockedUseQuestionSetFilterStore.mockReturnValue({
+      setSelectedTag: setSelectedTagMock,
+      selectedTag: 'TEST',
+    });
     render(<TagInputForm />);
-    screen.getByRole('form', { name: 'tag-input-form' });
-    screen.getByPlaceholderText('태그를 입력해주세요.');
-    screen.getByRole('button', { name: '조회' });
+  });
+
+  test('form submit 시 setSelectedTag가 호출된다', async () => {
+    const input = screen.getByLabelText('tag-input');
+    const form = screen.getByRole('form', { name: 'tag-input-form' });
+
+    await userEvent.type(input, '성공');
+    fireEvent.submit(form);
+
+    expect(setSelectedTagMock).toHaveBeenCalledWith('성공');
+  });
+
+  test('TagInputForm 컴포넌트가 렌더링되어야 합니다.', () => {
+    expect(
+      screen.getByRole('form', { name: 'tag-input-form' }),
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText('tag-input')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '조회' })).toBeInTheDocument();
   });
 });
--- frontend/src/pages/interviewQuestionsPage/ui/TagInputForm.tsx
+++ frontend/src/pages/interviewQuestionsPage/ui/TagInputForm.tsx
@@ -1,9 +1,28 @@
+import { useRef } from 'react';
+import useQuestionSetFilterStore from '@/pages/interviewQuestionsPage/model/useQuestionSetFilterStore';
+import { TAG_FILTER_LIST } from '@/pages/interviewQuestionsPage/model/constants';
+
 function TagInputForm() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { setSelectedTag } = useQuestionSetFilterStore();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const tag = inputRef.current?.value;
+    setSelectedTag(tag ?? TAG_FILTER_LIST[0].tag);
+  };
+
   return (
-    <form aria-label="tag-input-form" className="flex w-full gap-2">
+    <form
+      aria-label="tag-input-form"
+      className="flex w-full gap-2"
+      onSubmit={handleSubmit}
+    >
       <input
+        ref={inputRef}
         type="text"
         placeholder="태그를 입력해주세요."
+        aria-label="tag-input"
         className="border-point-400 text-point flex-1 rounded-md border p-2 focus:outline-none"
       />
       <button
