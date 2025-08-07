8d7dcb518f3ecb6626f05a1e937f6f61a5e71ff5
today-is-first <diligent0644@gmail.com>
2025-08-07 15:41:19 +0900

    test: 태그 클릭 시 제거 테스트 추가

--- frontend/src/entities/questionSet/test/TagDisplay.test.tsx
+++ frontend/src/entities/questionSet/test/TagDisplay.test.tsx
@@ -1,11 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import TagDisplay from '@/entities/questionSet/ui/TagDisplay';
+import userEvent from '@testing-library/user-event';
 
 describe('TagDisplay', () => {
+  const onClick = jest.fn();
+  beforeEach(() => {
+    const tags = ['tag1', 'tag2', 'tag3'];
+    render(<TagDisplay tags={tags} onClick={onClick} />);
+  });
+
   test('TagDisplay이 렌더링 된다.', () => {
-    render(<TagDisplay tags={['tag1', 'tag2', 'tag3']} />);
     expect(screen.getByText('#tag1')).toBeInTheDocument();
     expect(screen.getByText('#tag2')).toBeInTheDocument();
     expect(screen.getByText('#tag3')).toBeInTheDocument();
+  });
+
+  test('TagDisplay의 태그를 클릭하면 onClick 함수가 호출된다.', async () => {
+    await userEvent.click(screen.getByText('#tag1'));
+    expect(onClick).toHaveBeenCalledWith('tag1');
   });
 });
