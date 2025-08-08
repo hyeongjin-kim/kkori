b166a4b59e42a2d993655013bf00cbba23f0b204
hjkim <kims0887@naver.com>
2025-08-08 14:58:22 +0900

    feat: 스크린 전환, 응답 시작시 녹화기능 구현

--- frontend/src/widgets/interviewSection/model/index.ts
+++ frontend/src/widgets/interviewSection/model/index.ts
@@ -1,12 +1,40 @@
 import { interviewStatus } from '@/entities/interviewRoom/model/useInterviewRoomStore';
 import { usePracticeSessionStore } from '@/shared/lib/usePracticeSessionStore';
+import useMediaStreamStore from '@/widgets/interviewSection/model/useMediaStreamStore';
 
 export function switchScreen() {
-  console.log('switchScreen');
+  useMediaStreamStore
+    .getState()
+    .setMainStreamType(
+      useMediaStreamStore.getState().mainStreamType === 'my' ? 'peer' : 'my',
+    );
+  useMediaStreamStore
+    .getState()
+    .setSubStreamType(
+      useMediaStreamStore.getState().subStreamType === 'my' ? 'peer' : 'my',
+    );
 }
 
 export function startAnswer() {
-  console.log('startAnswer');
+  const myMediaStream = useMediaStreamStore.getState().myStream;
+  if (!myMediaStream) return;
+  const recorder = new MediaRecorder(myMediaStream);
+  useMediaStreamStore.getState().setMyRecorder(recorder);
+  let data: BlobPart[] = [];
+  recorder.ondataavailable = event => {
+    data.push(event.data);
+  };
+  recorder.onstop = () => {
+    const blob = new Blob(data, { type: 'audio/webm' });
+    const url = URL.createObjectURL(blob);
+  };
+  recorder.start();
+  setTimeout(() => {
+    if (recorder.state === 'recording') {
+      recorder.stop();
+    }
+  }, 10000);
+  usePracticeSessionStore.getState().answerStart();
 }
 
 export function endAnswer() {
