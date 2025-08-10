a0560bdcca3badc50171b7ed3da536baaead964b
hyeongjin-kim <kims0887@naver.com>
2025-08-10 23:08:45 +0900

    feat: 다음 질문 버튼 구현

---
+++ frontend/src/widgets/interviewSection/ui/NextQuestionButton.tsx
@@ -0,0 +1,19 @@
+function NextQuestionButton({
+  nextQuestion,
+  onClick,
+}: {
+  nextQuestion: string;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      aria-label="next-question-button"
+      onClick={onClick}
+      className="text-md inline-flex items-center rounded-2xl border border-transparent bg-blue-100 px-4 py-2 font-bold text-gray-700 shadow-sm transition-[background-color,border-color,transform] duration-150 hover:border-blue-400 hover:bg-blue-300 hover:text-black disabled:opacity-50"
+    >
+      {nextQuestion}
+    </button>
+  );
+}
+
+export default NextQuestionButton;
