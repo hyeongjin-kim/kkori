3bd5a6e4d8b8b192d3826849eac42530aec092c9
hyeongjin-kim <kims0887@naver.com>
2025-08-10 23:59:34 +0900

    feat: 인터뷰룸 스토어에 역할, 인터뷰 타입 추가

--- frontend/src/entities/interviewRoom/model/useInterviewRoomStore.tsx
+++ frontend/src/entities/interviewRoom/model/useInterviewRoomStore.tsx
@@ -14,22 +14,30 @@ export const interviewStatus = {
 
 interface InterviewRoomState {
   status: (typeof interviewStatus)[keyof typeof interviewStatus];
+  role: 'interviewee' | 'interviewer';
+  interviewType: 'solo' | 'pair';
 }
 
 interface InterviewRoomActions {
   setStatus: (
     status: (typeof interviewStatus)[keyof typeof interviewStatus],
   ) => void;
+  setRole: (role: 'interviewee' | 'interviewer') => void;
+  setInterviewType: (interviewType: 'solo' | 'pair') => void;
 }
 
 const initialState: InterviewRoomState = {
   status: interviewStatus.BEFORE_INTERVIEW,
+  role: 'interviewee',
+  interviewType: 'solo',
 };
 
 const useInterviewRoomStore = create<InterviewRoomState & InterviewRoomActions>(
   set => ({
     ...initialState,
     setStatus: status => set({ status }),
+    setRole: role => set({ role }),
+    setInterviewType: interviewType => set({ interviewType }),
   }),
 );
 
