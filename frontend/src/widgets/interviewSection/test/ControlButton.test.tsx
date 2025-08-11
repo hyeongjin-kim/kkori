23bc66c9b55b987778c2390a4e76430d9d5c10bc
hjkim <kims0887@naver.com>
2025-08-11 13:39:29 +0900

    refactor: 컨트롤 버튼이 렌더링 될 상태를 외부에서 주입하도록 변경

--- frontend/src/widgets/interviewSection/test/ControlButton.test.tsx
+++ frontend/src/widgets/interviewSection/test/ControlButton.test.tsx
@@ -1,18 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import ControlButton from '@/widgets/interviewSection/ui/ControlButton';
 import userEvent from '@testing-library/user-event';
+import useInterviewRoomStore from '@/entities/interviewRoom/model/useInterviewRoomStore';
 
 describe('ControlButton', () => {
   const testFunction = jest.fn();
 
   beforeEach(() => {
-    render(<ControlButton onClick={testFunction} label="test" text="test" />);
+    useInterviewRoomStore.setState({
+      status: 'BEFORE_INTERVIEW',
+    });
+    render(
+      <ControlButton
+        onClick={testFunction}
+        label="test"
+        text="test"
+        status="BEFORE_INTERVIEW"
+      />,
+    );
   });
 
-  test('화면 전환 버튼이 렌더링 된다.', () => {
+  test('컨트롤 버튼이 렌더링 된다.', () => {
     expect(screen.getByLabelText('test-control-button')).toBeInTheDocument();
   });
-  test('화면 전환 버튼이 클릭되면 switchScreen 함수가 호출된다.', async () => {
+  test('컨트롤 버튼이 클릭되면 onClick 함수가 호출된다.', async () => {
     const testButton = screen.getByLabelText('test-control-button');
     await userEvent.click(testButton);
     expect(testFunction).toHaveBeenCalled();
