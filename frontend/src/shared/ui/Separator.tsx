4f487d4dca7bfb9a222603ebf2e6a9aad6a73678
today-is-first <diligent0644@gmail.com>
2025-08-11 16:20:06 +0900

    feat: Separator 컴포넌트 구현

--- frontend/src/shared/ui/Separator.tsx
+++ frontend/src/shared/ui/Separator.tsx
@@ -1,5 +1,7 @@
 function Separator() {
-  return <div className="w-full border-t border-gray-200" role="separator" />;
+  return (
+    <div className="mb-4 w-full border-t border-gray-200" role="separator" />
+  );
 }
 
 export default Separator;
