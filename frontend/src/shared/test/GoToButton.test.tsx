56841ce5338a0d810f21d4949cb7be4301770c9c
today-is-first <diligent0644@gmail.com>
2025-08-11 12:02:48 +0900

    fix: 테스트 수정

--- frontend/src/shared/test/GoToButton.test.tsx
+++ frontend/src/shared/test/GoToButton.test.tsx
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import GoToButton from '@/shared/ui/GoToButton';
+import userEvent from '@testing-library/user-event';
+import MemoryRouterWrapped from '@/app/routes/MemoryRouterWrapped';
 
 describe('GoToButton', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouterWrapped
+        component={
+          <GoToButton
+            to="/question-set-create"
+            label="go-to-button"
+            text="test"
+          />
+        }
+      />,
+    );
+  });
   test('GoToButton 컴포넌트가 렌더링 된다.', () => {
-    render(<GoToButton to="/" label="go-to-button" text="test" />);
     expect(
       screen.getByRole('link', { name: 'go-to-button' }),
+    ).toBeInTheDocument();
+  });
+
+  test('GoToButton 컴포넌트를 클릭하면 해당 경로로 이동한다.', async () => {
+    const button = screen.getByRole('link', { name: 'go-to-button' });
+    await userEvent.click(button);
+    expect(
+      screen.getByRole('main', { name: 'question-set-create-page' }),
     ).toBeInTheDocument();
   });
 });
