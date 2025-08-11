7a5d8126b2df5fce46f78b814e3c26a58addd690
today-is-first <diligent0644@gmail.com>
2025-08-12 05:17:10 +0900

    refactor: QuestionSet 리팩토링

--- frontend/src/pages/interviewQuestionsPage/test/QuestionSet.test.tsx
+++ frontend/src/pages/interviewQuestionsPage/test/QuestionSet.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import QuestionSet from '@/pages/interviewQuestionsPage/ui/QuestionSet';
 import { questionSetList } from '@/entities/questionSet/model/mock';
-import { MemoryRouter } from 'react-router-dom';
+import MemoryRouterWrapped from '@/app/routes/MemoryRouterWrapped';
 
 describe('QuestionSet', () => {
   test('QuestionSet 컴포넌트가 렌더링되어야 합니다.', () => {
     render(
-      <MemoryRouter>
-        <QuestionSet questionSet={questionSetList[0]} />
-      </MemoryRouter>,
+      <MemoryRouterWrapped
+        component={<QuestionSet questionSet={questionSetList[0]} />}
+      />,
     );
-    screen.getByRole('listitem', { name: 'question-set' });
+    expect(screen.getByLabelText('question-set')).toBeInTheDocument();
   });
 });
