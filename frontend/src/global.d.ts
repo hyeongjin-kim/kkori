01671d0dda1779cefbab1125249f54f7b4cdf0fd
today-is-first <diligent0644@gmail.com>
2025-08-12 02:10:56 +0900

    chore: 테스트 파일 모킹 추가

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.webp';
