2c916721697b2596d529ab6d51c21e594f6d5b90
hyeongjin-kim <kims0887@naver.com>
2025-08-11 23:33:03 +0900

    feat: 다음 질문 모달 추가

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,6 +2,7 @@ import '@/app/styles/App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from '@/app/routes/AppRouter';
 import { QueryProvider } from '@/app/providers/QueryProvider';
+import NextQuestionModal from '@/widgets/interviewSection/ui/NextQuestionModal';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <AppRouter />
       </Router>
+      <NextQuestionModal />
     </QueryProvider>
   );
 }
