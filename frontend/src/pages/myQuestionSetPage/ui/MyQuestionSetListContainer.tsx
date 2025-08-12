70090a805fd91774c7c7fef5fe49ea81a6d647f9
today-is-first <diligent0644@gmail.com>
2025-08-12 23:46:47 +0900

    fix: 내 질문 세트 조회로 수정

--- frontend/src/pages/myQuestionSetPage/ui/MyQuestionSetListContainer.tsx
+++ frontend/src/pages/myQuestionSetPage/ui/MyQuestionSetListContainer.tsx
@@ -1,13 +1,8 @@
-import { useQuestionSets } from '@/entities/questionSet/model/useQuestionSetList';
-import useQuestionSetFilterStore from '@/pages/interviewQuestionsPage/model/useQuestionSetFilterStore';
+import { useMyQuestionSets } from '@/entities/questionSet/model/useQuestionSetList';
 import QuestionSetList from '@/pages/interviewQuestionsPage/ui/QuestionSetList';
-import { TAG_FILTER_LIST } from '@/pages/interviewQuestionsPage/model/constants';
 
 export default function MyQuestionSetListContainer() {
-  const selectedTag = useQuestionSetFilterStore(s => s.selectedTag);
-  const { data, isLoading, isError } = useQuestionSets({
-    tags: selectedTag === TAG_FILTER_LIST[0].tag ? undefined : [selectedTag],
-  });
+  const { data, isLoading, isError } = useMyQuestionSets({});
 
   if (isError) return <div className="p-4 text-red-500">불러오기 실패</div>;
 
