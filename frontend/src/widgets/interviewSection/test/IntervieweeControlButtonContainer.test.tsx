ede4967e7d2247b9eb62458b8b744babc88c2921
hjkim <kims0887@naver.com>
2025-08-13 15:41:06 +0900

    test: 같이 연습하기용 컨트롤 버튼 테스트 추가

--- frontend/src/widgets/interviewSection/test/IntervieweeControlButtonContainer.test.tsx
+++ frontend/src/widgets/interviewSection/test/IntervieweeControlButtonContainer.test.tsx
@@ -74,4 +74,51 @@ describe('IntervieweeControlButtonContainer', () => {
       ).toBeInTheDocument();
     });
   });
+  describe('Pair Mode', () => {
+    beforeEach(() => {
+      useInterviewRoomStore.getState().setType(interviewType.PAIR);
+      useInterviewRoomStore
+        .getState()
+        .setStatus(interviewStatus.BEFORE_INTERVIEW);
+      useInterviewRoomStore.getState().setRole(interviewRole.INTERVIEWEE);
+    });
+    test('화면 전환 버튼은 항상 렌더링 되어야 한다.', () => {
+      expect(
+        screen.getByLabelText('screen-change-control-button'),
+      ).toBeInTheDocument();
+    });
+    test('면접 종료 버튼은 항상 렌더링 되어야 한다.', () => {
+      expect(
+        screen.getByLabelText('interview-end-control-button'),
+      ).toBeInTheDocument();
+    });
+    test('면접 시작 전에는 면접 시작 버튼이 렌더링 되어야 한다.', () => {
+      useInterviewRoomStore
+        .getState()
+        .setStatus(interviewStatus.BEFORE_INTERVIEW);
+      expect(
+        screen.getByLabelText('interview-start-control-button'),
+      ).toBeInTheDocument();
+    });
+    test('면접이 시작되면 답변하기 버튼이 렌더링 되어야 한다.', () => {
+      act(() => {
+        useInterviewRoomStore
+          .getState()
+          .setStatus(interviewStatus.QUESTION_PRESENTED);
+      });
+      expect(
+        screen.getByLabelText('answer-start-control-button'),
+      ).toBeInTheDocument();
+    });
+    test('답변을 시작하면 답변 종료 버튼이 렌더링 되어야 한다.', () => {
+      act(() => {
+        useInterviewRoomStore
+          .getState()
+          .setStatus(interviewStatus.ANSWER_START);
+      });
+      expect(
+        screen.getByLabelText('answer-end-control-button'),
+      ).toBeInTheDocument();
+    });
+  });
 });
