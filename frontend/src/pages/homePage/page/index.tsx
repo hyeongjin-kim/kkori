15218cadd25659bd875393d7abd5cf8bd711c9a6
hjkim <kims0887@naver.com>
2025-08-13 12:54:42 +0900

    feat: 홈에서 미디어 스트림 설정

--- frontend/src/pages/homePage/page/index.tsx
+++ frontend/src/pages/homePage/page/index.tsx
@@ -3,8 +3,10 @@ import PairPracticeButton from '@/pages/homePage/ui/PairPracticeButton';
 import ThumbnailContainer from '@/pages/homePage/ui/ThumbnailContainer';
 import BackgroundShadow from '@/pages/homePage/ui/BackgroundShadow';
 import LeftSection from '@/pages/homePage/ui/LeftSection';
+import useInitMediaStream from '@/widgets/interviewSection/model/useInitMediaStream';
 
 function HomePage() {
+  const { error } = useInitMediaStream();
   return (
     <main
       aria-label="home-page"
