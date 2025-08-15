36ce50a7bbca95eb00060d403d4364f74fa25b44
hyeongjin-kim <kims0887@naver.com>
2025-08-15 21:39:25 +0900

    refactor: 공용 모달 컴포넌트로 다음 모달 컴포넌트 구현

--- frontend/src/widgets/interviewSection/ui/NextQuestionModal.tsx
+++ frontend/src/widgets/interviewSection/ui/NextQuestionModal.tsx
@@ -5,40 +5,63 @@ import {
   chooseDefaultQuestion,
   chooseTailQuestion,
 } from '../model';
-import useInterviewRoomStore from '@/entities/interviewRoom/model/useInterviewRoomStore';
+import Modal from '@/shared/ui/Modal';
 
-function NextQuestionModal() {
+function NextQuestionModal({
+  onClose,
+  contentRef,
+}: {
+  onClose: () => void;
+  contentRef: React.RefObject<HTMLDivElement | null>;
+}) {
   const { tailQuestion, defaultQuestion, customQuestion } =
     useInterviewQuestionStore();
-  const { modalOpen } = useInterviewRoomStore();
-  if (!modalOpen) return null;
+
   return (
-    <div
-      aria-label="next-question-modal"
-      className="absolute top-1/2 left-1/2 z-999 flex h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-2xl border border-gray-200 bg-white p-4 px-5 py-2 shadow-sm"
+    <Modal
+      title="다음 질문을 선택하세요"
+      subtitle="커스텀 질문을 선택하면 직접 질문을 만들 수 있습니다."
+      onClose={onClose}
+      contentRef={contentRef}
     >
-      <span className="text-2xl font-bold">다음 질문을 선택하세요</span>
-      <NextQuestionButton
-        nextQuestion={tailQuestion[0].question}
-        label="tail-question-1"
-        onClick={() => chooseTailQuestion(0)}
-      />
-      <NextQuestionButton
-        nextQuestion={tailQuestion[1].question}
-        label="tail-question-2"
-        onClick={() => chooseTailQuestion(1)}
-      />
-      <NextQuestionButton
-        nextQuestion={defaultQuestion.question}
-        label="default-question"
-        onClick={chooseDefaultQuestion}
-      />
-      <NextQuestionButton
-        nextQuestion={customQuestion}
-        label="custom-question"
-        onClick={chooseCustomQuestion}
-      />
-    </div>
+      <div
+        aria-label="next-question-modal"
+        className="flex flex-col gap-6 p-4 px-5 py-2"
+      >
+        <NextQuestionButton
+          nextQuestion={tailQuestion[0].question}
+          label="tail-question-1"
+          onClick={() => {
+            chooseTailQuestion(0);
+            onClose();
+          }}
+        />
+        <NextQuestionButton
+          nextQuestion={tailQuestion[1].question}
+          label="tail-question-2"
+          onClick={() => {
+            chooseTailQuestion(1);
+            onClose();
+          }}
+        />
+        <NextQuestionButton
+          nextQuestion={defaultQuestion.question}
+          label="default-question"
+          onClick={() => {
+            chooseDefaultQuestion();
+            onClose();
+          }}
+        />
+        <NextQuestionButton
+          nextQuestion={customQuestion}
+          label="custom-question"
+          onClick={() => {
+            chooseCustomQuestion();
+            onClose();
+          }}
+        />
+      </div>
+    </Modal>
   );
 }
 
