99737f1891dde40e570084ae14a368da3e2c0e0f
hyeongjin-kim <kims0887@naver.com>
2025-08-17 12:57:55 +0900

    feat: 상태 변경 및 역할 변경시 토스트 알림 기능 추가

--- frontend/src/entities/interviewRoom/model/useInterviewRoomStore.tsx
+++ frontend/src/entities/interviewRoom/model/useInterviewRoomStore.tsx
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
+import useInterviewToastStore from '@/widgets/interviewSection/model/useInterviewToastStore';
 
 export const interviewStatus = {
   BEFORE_INTERVIEW: 'beforeInterview',
@@ -41,6 +42,21 @@ interface InterviewRoomActions {
   setType: (type: (typeof interviewType)[keyof typeof interviewType]) => void;
 }
 
+const statusToToastMessage = {
+  [interviewStatus.BEFORE_INTERVIEW]: '면접 시작 전 상태로 변경되었습니다.',
+  [interviewStatus.QUESTION_PRESENTED]: '질문 제시 되었습니다.',
+  [interviewStatus.ANSWER_START]: '답변 녹음을 시작했습니다.',
+  [interviewStatus.ANSWER_SUBMIT]:
+    '답변이 제출 되었습니다. 다음 질문을 준비합니다.',
+  [interviewStatus.NEXT_QUESTION_PRESENTED]: '다음 질문이 제시 되었습니다.',
+  [interviewStatus.NEXT_QUESTION_SELECTED]: '다음 질문이 선택 되었습니다.',
+  [interviewStatus.CUSTOM_QUESTION_SELECTED]: '커스텀 질문이 선택 되었습니다.',
+  [interviewStatus.CUSTOM_QUESTION_START]:
+    '커스텀 질문 녹음이 시작 되었습니다.',
+  [interviewStatus.CUSTOM_QUESTION_CREATED]: '커스텀 질문이 생성 되었습니다.',
+  [interviewStatus.END_INTERVIEW]: '면접이 종료 되었습니다.',
+};
+
 const initialState: InterviewRoomState = {
   status: interviewStatus.BEFORE_INTERVIEW,
   role: interviewRole.INTERVIEWEE,
@@ -53,8 +69,22 @@ const useInterviewRoomStore = create<
   persist(
     set => ({
       ...initialState,
-      setStatus: status => set({ status }),
-      setRole: role => set({ role }),
+      setStatus: status => {
+        set({ status });
+        useInterviewToastStore
+          .getState()
+          .addToast(statusToToastMessage[status]);
+      },
+      setRole: role => {
+        set({ role });
+        useInterviewToastStore
+          .getState()
+          .addToast(
+            role === interviewRole.INTERVIEWER
+              ? '역할이 면접관으로 변경되었습니다.'
+              : '역할이 면접자로 변경되었습니다.',
+          );
+      },
       setType: type => set({ type }),
     }),
     {
